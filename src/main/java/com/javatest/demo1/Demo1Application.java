d9d5caedd99bf9d68ed315d6877e7943467ef040
lxh <deeperXH@gmail.com>
2018-02-02 16:36:33 +0800

    第二次提交

--- src/main/java/com/javatest/demo1/Demo1Application.java
+++ src/main/java/com/javatest/demo1/Demo1Application.java
@@ -2,11 +2,22 @@ package com.javatest.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Demo1Application {
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Demo1Application.class, args);
 	}
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addRedirectViewController("/", "/readingList");
+//	}
+
+
 }
--- src/main/java/com/javatest/demo1/ReadingListController.java
+++ src/main/java/com/javatest/demo1/ReadingListController.java
@@ -4,6 +4,7 @@ import antlr.collections.impl.LList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
-
 @RequestMapping("/readingList")
 public class ReadingListController {
+
+    private static  final String reader="craig";
 
     private ReadlinesRepository readlinesRepository;
 
@@ -22,6 +24,12 @@ public class ReadingListController {
         this.readlinesRepository = readlinesRepository;
     }
 
+    @GetMapping("")
+    public String index(Model model) {
+        System.out.println("开始");
+        model.addAttribute("books", new Book());
+        return "readingList";
+    }
     @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
 
     public String readersBooks(@PathVariable("reader") String reader, Model model) {
